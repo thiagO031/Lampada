cd07980eaf8b96757f5cf97dad67e91c5fd79efb
thiagO031 <thiago.tsantana3@gmail.com>
2022-04-13 17:28:53 -0300

    Em processo

---
+++ script.js
@@ -0,0 +1,14 @@
+let img = document.getElementById('imagem')
+
+function ligar(){
+    img.src="./img/lampada_lig.jpg"
+}
+
+function quebrar(){
+    img.src="./img/lampada_queb.jpg"
+    
+}
+
+function resetar(){
+    img.src="./img/lampada_desl.jpg"
+}
